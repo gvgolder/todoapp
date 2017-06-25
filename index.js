b995ca15b579133b566f1729b70727720d0f3bda
Viktor Goncharenko <gvgolder@gmail.com>
2017-06-25 17:51:58 +0300

    pisoses

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const crud = require('./crud')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/', crud.readTask)
+app.post('/', crud.createTask)
+app.put('/', crud.updateTask)
+app.delete('/', crud.removeTask)
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
